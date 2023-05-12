7e51ae0f93b068e77ba8d147e732577c81155431
Lovro Zelić <lovro.zelic@icloud.com>
2023-05-12 13:34:24 +0200

    run migration

--- db/migrate/20230512112517_create_zombies.rb
+++ db/migrate/20230512112517_create_zombies.rb
@@ -1,11 +1,12 @@
 class CreateZombies < ActiveRecord::Migration[7.0]
   def change
-    create_table :zombies do |t|
+    create_table :zombies do |t| # automatically creates a PRIMARY KEY called ID
       t.string :name
       t.text :bio
       t.integer :age
 
-      t.timestamps
+      t.timestamps # SAME AS: t.datetime :created_at
+                   #          t.datetime :updated_at
     end
   end
 end
