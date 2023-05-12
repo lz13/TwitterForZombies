3b9fc6128bf621ba5900d1e874ef1ebfee59e075
Lovro Zelić <lovro.zelic@icloud.com>
2023-05-12 15:24:21 +0200

    make rotting zombies

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,2 +1,12 @@
 class Zombie < ApplicationRecord
+
+  scope :rotting, -> { where(rotting: true) }
+  scope :fresh, -> { where("age < 20") }
+  scope :recent, -> { order("created_at desc").limit(3) }
+
+  before_save :make_rotting
+
+  def make_rotting
+    self.rotting = true if age > 20
+  end
 end
